40a7ccd320525f2d3109ab9d3a184a50723aa664
Ricky <ricard.ferrero@databot.es>
2024-01-28 14:31:59 +0100

    basic algorithm working

--- modules/constants.py
+++ modules/constants.py
@@ -1,6 +1,9 @@
 from pygame import Rect as R
 from pygame import draw as d
 
+X = 0
+Y = 1
+
 W_WIDTH = 1280
 W_HEIGHT = 720
 WINDOW = (W_WIDTH, W_HEIGHT)
@@ -9,6 +12,7 @@ RECT_WIDTH = 250
 RECT_HEIGHT = 100
 
 class Rect():
+    FACTOR = 0.1
     def __init__(self, rect_dict):
         self.name = rect_dict['name']
         self.color = rect_dict['color']
@@ -19,7 +23,7 @@ class Rect():
             'height': rect_dict['height']
         }
         self.re_init()
-    
+
     def re_init(self):
         self.left = self.init_coordenates['left']
         self.top = self.init_coordenates['top']
@@ -30,15 +34,23 @@ class Rect():
         # pg.draw.rect(screen, color, rect_form)
         d.rect(surface, self.color, self.__rect())
 
-    def re_construct(self, vp): # vp => Vanishing Point (x, y)
-        pass
+    def do_zoom(self, vp, direction): # vp => Vanishing Point (x, y)
+        if direction == 'in':
+            self.__zoom_it(vp, 1)
+        elif direction == 'out':
+            self.__zoom_it(vp, -1)
+        else:
+            raise ValueError(f'DoZoom: direction must be "in" or "out" {self}')
+
+
+    ### PRIVATE
 
     # Returns a dot coordenates (x,y). It represents
     # one of the 4 corners.
     # Described by
     # row (0=> top corners, 1=> bottom corners)
     # col (0=> left corners, 1=> right corners)
-    def __corner(self, row, col):
+    def __get_corner(self, row, col):
         if row!=0 and row!=1 or col!=0 and col!=1:
             raise ValueError(f'Corner: incorrect coordenates {self}')
         x = self.left + self.width * col
@@ -47,14 +59,48 @@ class Rect():
 
     def __rect(self):
         # Rect(left, top, width, height)
-        return R(self.left, self.top, self.width, self.height)
+        return R(int(self.left), int(self.top), int(self.width), int(self.height))
 
     def __get_vector(self, vp, corner): # vp => Vanishing Point (x, y)
-        X = 0
-        Y = 1
-        x_vector = corner[X] - vp[X]
-        y_vector = corner[Y] - vp[Y]
-        return (x_vector, y_vector)
+        x = corner[X] - vp[X]
+        y = corner[Y] - vp[Y]
+        return (x, y)
+
+    def __get_zoom_vector(self, vector, direction):
+        x = round(vector[X] * self.FACTOR * direction, 2)
+        y = round(vector[Y] * self.FACTOR * direction, 2)
+        return (x, y)
+
+    # DoZoom needs only two corners:
+    # Corner(0,0) -> to manipulate 'top' and 'left'
+    # Corner(1,1) -> to manipulate 'height' and 'width'
+    def __zoom_it(self, vp, direction): # vp => Vanishing Point (x, y) | direction: +1=> zoom in, -1=> zoom out
+        if direction != 1 and direction != -1:
+            raise ValueError(f'Zoom: direction must be 1 or -1 {self}')
+        # Get important corners (to manipulate coordinates of the rectangle)
+        corner_00 = self.__get_corner(0,0)
+        corner_11 = self.__get_corner(1,1)
+        # Get important vectors from important corners (to manipulate the coordinates of both corners)
+        vector_00 = self.__get_vector(vp, corner_00)
+        vector_11 = self.__get_vector(vp, corner_11)
+        # Get zoom vectors (are the vectors to increase or decrase the coordinates of both corners)
+        z_vector_00 = self.__get_zoom_vector(vector_00, direction)
+        z_vector_11 = self.__get_zoom_vector(vector_11, direction)
+
+        # Corner(0,0) => manipulate 'left' and 'top'
+        new_left = self.left + z_vector_00[X]
+        new_top = self.top + z_vector_00[Y]
+        # Corner(1,1) => manipulate 'width' and 'height'
+        new_width = (self.left + self.width) + z_vector_11[X] - new_left
+        new_height = (self.top + self.height) + z_vector_11[Y] - new_top
+
+        self.left = new_left
+        self.top = new_top
+        self.width = new_width
+        self.height = new_height
+
+
+
 
 rect_red = Rect({
     'name': 'RED',
--- zoom_algorithm.py
+++ zoom_algorithm.py
@@ -9,20 +9,41 @@ def main():
     screen = pg.display.set_mode(WINDOW)
     clock = pg.time.Clock()
     running = True
+    zoom = False
+    reset = False
 
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    # Zoom In
+                    zoom = 'in'
+                if event.key == pg.K_DOWN:
+                    # Zoom Out
+                    zoom = 'out'
+                if event.key == pg.K_r:
+                    reset = True
+
 
         # fill the screen with a color to wipe away anything from last frame
         screen.fill("white")
 
-        # RENDER YOUR GAME HERE
 
+        # RENDER YOUR GAME HERE
         for rect in rects:
+            # Reset if user pushes 'R'
+            if reset:
+                rect.re_init()
+            # Zoom if user pushes arrows (up or down)
+            if zoom:
+                rect.do_zoom(pg.mouse.get_pos(), zoom)
+            # Print the results
             rect.draw(screen)
 
+        reset = False
+        zoom = False
         # flip() the display to put your work on screen
         pg.display.flip()
 
